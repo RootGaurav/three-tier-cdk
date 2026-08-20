b5683537170b7214387cef28e764b81903f0539b
RootGaurav <gauravrawat0259@gmail.com>
2026-08-20 11:38:21 +0530

    Initial CDK Project

---
+++ test/aws-cdk-gitaction-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkGitactionInfra from '../lib/aws-cdk-gitaction-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-gitaction-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkGitactionInfra.AwsCdkGitactionInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
